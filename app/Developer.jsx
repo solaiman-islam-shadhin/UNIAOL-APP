552dd4d456016c67a3a4c16a5abc0d3ce861011f
solaiman-islam-shadhin <solaimanislamshadhin123@gmail.com>
2025-08-15 22:24:56 +0600

    12th refine

--- app/Developer.jsx
+++ app/Developer.jsx
@@ -9,7 +9,7 @@ import Animated, { FadeInUp } from 'react-native-reanimated';
 const developers = [
     {
         id: '1',
-        name: 'MD Solaiman Islam Shadhin',
+        name: 'Md Solaiman Islam Shadhin',
         description: 'Lead React Native and React JS developer specializing in UI/UX and full stack Mobile app and Web development',
         image: 'https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-6/491932379_2144108269387914_8599008879674339284_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeHlIJdXTkU1M70uY0mMwv9vLkPMDAqo2WwuQ8wMCqjZbJbSWEwzHX7bX5tVTbv-W8BmbwHpps4jxHfkP_nQMle_&_nc_ohc=ajGhvBXsoaMQ7kNvwHk5Jfy&_nc_oc=AdmV-72yH17yF_Tf5D-8nvVCwOK_PATYQcXAhdpk4w2INCAG8f1-kKpJ4_a519Ora4DzkNgBhDRBtB69O6Mi_GYL&_nc_zt=23&_nc_ht=scontent-sin6-2.xx&_nc_gid=cmfyIJhy-m9gt9mPE5aAyA&oh=00_AfU6bxsDSoVPIm9UZH6mQuYFk1UqEk_BYYdRMCIOAz2QLA&oe=68A3B9E2',
     },
@@ -21,13 +21,13 @@ const developers = [
     },
     {
         id: '3',
-        name: 'MD Mehedi Hasan',
+        name: 'Md Mahamud Hasan',
         description: 'Firebase specialist and developer.',
         image: 'https://media-sin11-1.cdn.whatsapp.net/v/t61.24694-24/363035589_6401489473294078_4682999899759549692_n.jpg?ccb=11-4&oh=01_Q5Aa2QGScx7e-D9myD0soOkmx5h5bY43I5UHXyc1JHYkJoGSMg&oe=68AB0FA4&_nc_sid=5e03e0&_nc_cat=104',
     },
     {
         id: '4',
-        name: 'MD Tanvir Alam',
+        name: 'Md Tanvir Alam',
         description: 'UI/UX designer and front-end developer.',
         image: 'https://media-sin2-1.cdn.whatsapp.net/v/t61.24694-24/315763494_545296157028687_2937590678254268355_n.jpg?ccb=11-4&oh=01_Q5Aa2QFHmT5Lau05ta3V4t3Zgx5ASKX0Hc2bGdFEBTNReFF4PA&oe=68AB1DBD&_nc_sid=5e03e0&_nc_cat=100',
     },
